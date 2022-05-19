9b666ce2ddca7cd3868cc7e319f1704631c5e3b1
Farhad Khodaei <39498931+farhadkhodaei@users.noreply.github.com>
2022-05-19 21:50:21 +0430

    Add files via upload

--- DC load flow/busvalues.m
+++ DC load flow/busvalues.m
@@ -7,14 +7,16 @@ basemva = 100;  accuracy = 0.0001;  maxiter = 10;
 %       Bus  -Load- -Gen-
 %       No.    MW     MW  
 busdata=[2   340.0  200.0  
-         3   190.0    0.0];
+         3   190.0    0.0
+         4    30.0    0.0];
 
 %        The bus '1' is slack
 %        Bus   Bus     R        X       1/2B
 %        No.   No.    p.u.     p.u.     p.u.
 linedata=[1     2     0.00     0.1     0.0000    
           1     3     0.00     0.2     0.0000    
-          2     3     0.00     0.3     0.0000];
+          2     3     0.00     0.3     0.0000
+          3     4     0.00     0.1     0.0000];
 lfybus;    % Forms the bus admittance matrix
 Ybus
 lfdc       % does DC load flow
