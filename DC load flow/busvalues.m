f8910990f2fea9afe123f28838865ea2a6217cbe
Farhad Khodaei <39498931+farhadkhodaei@users.noreply.github.com>
2022-05-19 19:07:57 +0430

    Add files via upload

---
+++ DC load flow/busvalues.m
@@ -0,0 +1,19 @@
+clear all
+basemva = 100;  accuracy = 0.0001;  maxiter = 10;
+% The impedances are expressed on a 1000 MVA base.
+% In problems 9.7-9.9 the base is mistakenly stated as 100 MVA.
+% for non slack buses
+%       Bus  -Load- -Gen-
+%       No.    MW     MW  
+busdata=[1   340.0  200.0  
+         2   190.0    0.0];
+
+%        The bus '1' is slack
+%        Bus   Bus     R        X       1/2B
+%        No.   No.    p.u.     p.u.     p.u.
+linedata=[1     2     0.00     0.1     0.0000    
+          1     3     0.00     0.2     0.0000    
+          2     3     0.00     0.3     0.0000];
+lfybus;    % Forms the bus admittance matrix
+Ybus
+lfdc       % does DC load flow
---
+++ DC load flow/lfybus.m
@@ -0,0 +1,27 @@
+%  This program obtains th Bus Admittance Matrix for power flow solution
+%  Copyright (c) 1998 by H. Saadat
+
+j=sqrt(-1); i = sqrt(-1);
+nl = linedata(:,1); nr = linedata(:,2); R = linedata(:,3);
+X = linedata(:,4); Bc = j*linedata(:,5);
+nbr=length(linedata(:,1)); nbus = max(max(nl), max(nr));
+Z = R + j*X; y= ones(nbr,1)./Z;        %branch admittance
+for n = 1:nbr
+Ybus=zeros(nbus,nbus);     % initialize Ybus to zero
+               % formation of the off diagonal elements
+for k=1:nbr;
+       Ybus(nl(k),nr(k))=Ybus(nl(k),nr(k))-y(k);
+       Ybus(nr(k),nl(k))=Ybus(nl(k),nr(k));
+    end
+end
+              % formation of the diagonal elements
+for  n=1:nbus
+     for k=1:nbr
+         if nl(k)==n
+         Ybus(n,n) = Ybus(n,n)+y(k) + Bc(k);
+         elseif nr(k)==n
+         Ybus(n,n) = Ybus(n,n)+y(k) +Bc(k);
+         else, end
+     end
+end
+clear Pgg
