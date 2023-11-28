33c1375f10a9762febc4ce5c2778615e6f26aa09
barbarabadi <149609422+barbarabadi@users.noreply.github.com>
2023-11-28 12:34:57 +0330

    Update lfybus.m

--- DC load flow/lfybus.m
+++ DC load flow/lfybus.m
@@ -6,14 +6,14 @@ nl = linedata(:,1); nr = linedata(:,2); R = linedata(:,3);
 X = linedata(:,4); Bc = j*linedata(:,5);
 nbr=length(linedata(:,1)); nbus = max(max(nl), max(nr));
 Z = R + j*X; y= ones(nbr,1)./Z;        %branch admittance
-for n = 1:nbr
+
 Ybus=zeros(nbus,nbus);     % initialize Ybus to zero
                % formation of the off diagonal elements
 for k=1:nbr;
        Ybus(nl(k),nr(k))=Ybus(nl(k),nr(k))-y(k);
        Ybus(nr(k),nl(k))=Ybus(nl(k),nr(k));
-    end
 end
+
               % formation of the diagonal elements
 for  n=1:nbus
      for k=1:nbr
